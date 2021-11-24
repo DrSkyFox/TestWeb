d2d32f221f8564dead02053bb746002204481ba8
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-11-25 02:43:08 +0300

    add docker
    docker build -t test-web-simple:1.0.0
    docker run -d -p 8080:8080 -t  test-web-simple:1.0.0
    
    add default page as swagger doc

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM openjdk:17
+FROM openjdk:17-alpine
+RUN addgroup -S webtest && adduser -S webtest -G webtest
+USER webtest
 EXPOSE 8080
 ARG JAR_FILE=target/TestWeb-0.0.1-SNAPSHOT.jar
 WORKDIR opt/app
+LABEL custom.size=100
+      custom.msg="Hello, Mr. Unknow! Tell me.. Why u asking me"
 COPY ${JAR_FILE} testWeb.jar
 ENTRYPOINT ["java", "-jar", "testWeb.jar"]
---
+++ src/main/java/ru/test/controller/WebConfig.java
@@ -0,0 +1,21 @@
+package ru.test.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${springdoc.swagger-ui.path}")
+    private  String urlDefault;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(urlDefault);
+    }
+}
